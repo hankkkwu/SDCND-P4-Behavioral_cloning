0bb6500ab1d0775ad708ef3c9a8d317229dbd6c3
hankkkwu <two30022001@hotmail.com>
2019-05-04 23:16:57 +0800

    Change the driving speed in autonomous mode

--- drive.py
+++ drive.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
+import cv2
 
 import numpy as np
 import socketio
@@ -15,6 +16,7 @@ from io import BytesIO
 from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
+import tensorflow as tf
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -44,7 +46,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 17
 controller.set_desired(set_speed)
 
 
@@ -61,6 +63,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        #vimage_array = image_array[60:135,:,:]
+        #image_array = cv2.resize(image_array, (320,139))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
